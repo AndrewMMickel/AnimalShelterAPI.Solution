f5ee1724e72844e85d7e05e10cd95e5878e7df9c
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-07-26 12:29:53 -0700

    git init, forgot to do this

---
+++ AnimalAPI/Controllers/AnimalsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using AnimalAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private AnimalAPIContext _db;
+
+        public AnimalsController(AnimalAPIContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Animal>> Get(string name, string type, string gender, string behavior, string dateAdded)
+        {
+            var query = _db.Animals.AsQueryable();
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+            if (type != null)
+            {
+                query = query.Where(entry => entry.Type == type);
+            }
+            if (gender != null)
+            {
+                query = query.Where(entry => entry.Gender == gender);
+            }
+            if (behavior != null)
+            {
+                query = query.Where(entry => entry.Behavior == behavior);
+            }
+            if (dateAdded != null)
+            {
+                query = query.Where(entry => entry.DateAdded == dateAdded);
+            }
+            return query.ToList();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Animal animal)
+        {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Animal> Get(int id)
+        {
+            return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Animal animal)
+        {
+            animal.AnimalId = id;
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Animal animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+            _db.Animals.Remove(animalToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ AnimalAPI/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using AnimalAPI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace AnimalAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AnimalAPIContext>(opt =>
+                opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // how to make a custom swagger page.
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "AnimalShelter API";
+                    document.Info.Description = "A simple ASP.NET Core web API";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "David Boedigheimer",
+                        Email = "stealth259@gmail.com",
+                        Url = string.Empty
+                    };
+                    document.Info.License = new NSwag.OpenApiLicense
+                    {
+                        Name = "Use under MIT",
+                        Url = "https://example.com/license"
+                    };
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            // app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
